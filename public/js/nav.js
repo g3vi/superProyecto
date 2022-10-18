e84decb568710245a6d44b00f43a75ea49c96a9e
Gerardo Vieyra <g.vieyraaguirre@ugto.mx>
2022-10-18 11:38:30 -0500

    correccion de errores, imagenes y efectos agregados

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,6 +1,6 @@
 const navbar=document.querySelector('.navbar')
-window.addEventListener('scroll',()=>{
-    if(scrollY=>180){
+window.addEventListener('scroll', () => {
+    if(scrollY >= 180){
         navbar.classList.add('bg')
     }else{
         navbar.classList.remove('bg')
@@ -13,18 +13,12 @@ const createNavbar=()=>{
         <li class="link-item">
              <a href="#" class="link active">Home</a>
         </li>
-    </ul>
-    <ul class="links-item">
         <li class="link-item">
             <a href="#" class="link">Product</a>
         </li>
-    </ul>
-    <ul class="links-item>
         <li class="link-item">
             <a href="#" class="link">About</a>
         </li>
-    </ul>
-    <ul class="links-item">
         <li class="link-item">
             <a href="#" class="link">Contact</a>
         </li>
@@ -44,7 +38,7 @@ const createNavbar=()=>{
             <img src="./img/user.png" class="user-icon">
         <div class="user-icon-popup">
             <p>Login to your acount</p>
-            <p>Login</p>
+            <a>Login</a>
         </div>
         </div>
     </div>
